1b0b73ef4237d3bcc0801af5e9c18b778efdb959
Blueddo <Blueddo@users.noreply.github.com>
2022-03-14 21:58:19 +0200

    Add files via upload

---
+++ luaScr/PasswordManager/translations/translations/lang_el.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el_GR" sourcelanguage="en_US">
+<context>
+    <name>simpleTV::PasswordManager</name>
+    <message>
+        <source>edit</source>
+        <translation>επεξεργασία</translation>
+    </message>
+    <message>
+        <source>incorrect ID</source>
+        <translation>εσφαλμένη ταυτότητα</translation>
+    </message>
+    <message>
+        <source>error in the passwords database</source>
+        <translation>σφάλμα στη βάση δεδομένων κωδικών πρόσβασης</translation>
+    </message>
+    <message>
+        <source>ID is not found in the passwords database</source>
+        <translation>Το αναγνωριστικό δεν βρίσκεται στη βάση δεδομένων κωδικών πρόσβασης</translation>
+    </message>
+    <message>
+        <source>Enter login/password for </source>
+        <translation>Εισαγωγή σύνδεσης/κωδικού πρόσβασης για </translation>
+    </message>
+    <message>
+        <source>show password</source>
+        <translation>εμφάνιση κωδικού πρόσβασης</translation>
+    </message>
+    <message>
+        <source>delete</source>
+        <translation>διαγραφή</translation>
+    </message>
+    <message>
+        <source>Id</source>
+        <translation>Ταυτότητα</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Ονομα</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Σύνδεση</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Κωδικός πρόσβασης</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Νέο</translation>
+    </message>
+    <message>
+        <source>Preference</source>
+        <translation>Προτιμήσεις</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Ενέργειες</translation>
+    </message>
+    <message>
+        <source>Editor</source>
+        <translation>Συντάκτης</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Αποθήκευση</translation>
+    </message>
+    <message>
+        <source>Show passwords</source>
+        <translation>Εμφάνιση κωδικών πρόσβασης</translation>
+    </message>
+    <message>
+        <source>Show dialog of enter password</source>
+        <translation>Εμφάνιση παραθύρου διαλόγου εισαγωγής κωδικού πρόσβασης</translation>
+    </message>
+    <message>
+        <source>Import</source>
+        <translation>Εισαγωγή</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Εξαγωγή</translation>
+    </message>
+    <message>
+        <source>Can&apos;t open file</source>
+        <translation>Δεν είναι δυνατό το άνοιγμα του αρχείου</translation>
+    </message>
+    <message>
+        <source>Incorrect file</source>
+        <translation>Εσφαλμένο αρχείο</translation>
+    </message>
+    <message>
+        <source>Can&apos;t open destination file</source>
+        <translation>Δεν είναι δυνατό το άνοιγμα του αρχείου προορισμού</translation>
+    </message>
+    <message>
+        <source>No data for export</source>
+        <translation>Δεν υπάρχουν δεδομένα για εξαγωγή</translation>
+    </message>
+    <message>
+        <source>Export completed successfully</source>
+        <translation>Η εξαγωγή ολοκληρώθηκε με επιτυχία</translation>
+    </message>
+    <message>
+        <source>File saved in </source>
+        <translation>Το αρχείο αποθηκεύτηκε στο </translation>
+    </message>
+    <message>
+        <source>All current logins/passwords will be removed.
+Continue ?</source>
+        <translation>Όλες οι τρέχουσες συνδέσεις/κωδικοί πρόσβασης θα αφαιρεθούν.
+Συνέχεια?</translation>
+    </message>
+</context>
+</TS>
