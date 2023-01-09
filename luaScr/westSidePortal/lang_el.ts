0ec4201b55cc3b5b12fea350a47624db07bdc7a7
Maikkoum <Blueddo@users.noreply.github.com>
2023-01-09 23:54:15 +0200

    Add files via upload

---
+++ luaScr/westSidePortal/lang_el.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el_GR" sourcelanguage="en_US">
+<context>
+    <name>simpleTV::westSidePortal</name>
+    <message>
+        <source>Options for westSidePortal </source>
+        <translation>Επιλογές για το westSidePortal </translation>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translation>Ενεργοποιημένο</translation>
+    </message>
+    <message>
+        <source>Embedded in main frame</source>
+        <translation>Ενσωματωμένο στο κύριο πλαίσιο</translation>
+    </message>
+    <message>
+        <source>Save position</source>
+        <translation>Αποθήκευση θέσης</translation>
+    </message>
+    <message>
+        <source>Chromium mode</source>
+        <translation>Λειτουργία Chromium</translation>
+    </message>
+    <message>
+        <source>Addon Media portal for SimpleTV. Implements the ability to select media content for portal-connected sites based on dialog boxes.&lt;br&gt;Keyboard shortcuts: &apos;Ctrl+Shift+P&apos; - show OSD.</source>
+        <translation>Προσθήκη πύλης πολυμέσων για το SimpleTV. Εφαρμογή της δυνατότητας επιλογής περιεχομένου πολυμέσων για ιστότοπους που είναι συνδεδεμένοι με την πύλη με βάση πλαίσια διαλόγου.&lt;br&gt;Συντομεύσεις πληκτρολογίου: &apos;Ctrl + Shift + P&apos; - εμφάνιση στο OSD.</translation>
+    </message>
+</context>
+</TS>
